5a8ef428423b22737c32f156b53f19512a56333e
Joaquim-Claudio <116195116+Joaquim-Claudio@users.noreply.github.com>
2023-10-20 18:50:16 +0100

    Recreating the Android Application.

---
+++ MyPastry_PROJETOMOBILE/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MyPastry_PROJETO-MOBILE"
+include(":app")
+ 
