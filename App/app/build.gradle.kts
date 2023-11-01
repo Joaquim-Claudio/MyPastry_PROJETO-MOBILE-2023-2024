c5109b453ea51babc7b7117f078ceb8d4beda232
Joaquim-Claudio <116195116+Joaquim-Claudio@users.noreply.github.com>
2023-11-01 01:13:02 +0000

    Updating the CompilerSDK!

--- App/app/build.gradle.kts
+++ App/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "pt.iade.mypastry"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "pt.iade.mypastry"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
