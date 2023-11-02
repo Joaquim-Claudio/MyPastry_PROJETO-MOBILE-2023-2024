ee75b723f4cbebad079032d419f328d7c45d974e
Joaquim-Claudio <116195116+Joaquim-Claudio@users.noreply.github.com>
2023-11-02 01:37:28 +0000

    Created and finished the Sign In Activity.

---
+++ App/app/src/main/java/pt/iade/mypastry/SignInActivity.java
@@ -0,0 +1,14 @@
+package pt.iade.mypastry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
