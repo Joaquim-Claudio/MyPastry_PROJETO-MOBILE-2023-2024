84a9d861cf8da5cbd324d80ca4acaacfe140e2c4
Joaquim-Claudio <116195116+Joaquim-Claudio@users.noreply.github.com>
2023-11-02 02:26:16 +0000

    Created and Finished the Email Confirmation Activity.

---
+++ App/app/src/main/java/pt/iade/mypastry/EmailConfirmationActivity.java
@@ -0,0 +1,14 @@
+package pt.iade.mypastry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmailConfirmationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_confirmation);
+    }
+}
