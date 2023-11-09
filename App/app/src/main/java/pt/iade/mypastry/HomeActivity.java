90801ce15bdd5bea75f59fbed35da802b2f4579b
Joaquim-Claudio <116195116+Joaquim-Claudio@users.noreply.github.com>
2023-11-09 11:14:01 +0000

    Created the Menu Activity.

--- App/app/src/main/java/pt/iade/mypastry/HomeActivity.java
+++ App/app/src/main/java/pt/iade/mypastry/HomeActivity.java
@@ -2,7 +2,9 @@ package pt.iade.mypastry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void callMenuActiviy(View view){
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 }
