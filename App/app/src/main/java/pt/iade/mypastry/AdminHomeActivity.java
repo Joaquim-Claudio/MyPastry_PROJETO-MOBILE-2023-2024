61284e30a9ab89ff94d6dccfc97d61ce32b3842d
Joaquim-Claudio <116195116+Joaquim-Claudio@users.noreply.github.com>
2023-11-09 14:59:03 +0000

    Adding all the images.

---
+++ App/app/src/main/java/pt/iade/mypastry/AdminHomeActivity.java
@@ -0,0 +1,14 @@
+package pt.iade.mypastry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_activity_home);
+    }
+}
