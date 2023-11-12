76ea78468c48ca28d3d5c118be5f1f12d68b99d0
Joaquim-Claudio <116195116+Joaquim-Claudio@users.noreply.github.com>
2023-11-12 03:06:02 +0000

    Updating the Menu Activity.

--- App/app/src/main/java/pt/iade/mypastry/MenuActivity.java
+++ App/app/src/main/java/pt/iade/mypastry/MenuActivity.java
@@ -2,7 +2,9 @@ package pt.iade.mypastry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void returnToHomeActivity(View view){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
