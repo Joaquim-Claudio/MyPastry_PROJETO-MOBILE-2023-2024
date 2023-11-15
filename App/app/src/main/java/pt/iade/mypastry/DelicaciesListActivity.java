1b6c5dc6847b87e98c031fed17593e8770ec73c1
Laís Ferreira <laissferreira@icloud.com>
2023-11-15 01:30:40 +0000

    Creating delicacies list activity

---
+++ App/app/src/main/java/pt/iade/mypastry/DelicaciesListActivity.java
@@ -0,0 +1,14 @@
+package pt.iade.mypastry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DelicaciesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delicacies_list);
+    }
+}
